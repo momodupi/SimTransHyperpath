c08f45a5487bb4f78157e3afa56466a0d261bbfc
momodupi <momodupi@gmail.com>
2019-02-01 23:30:06 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 class Graph(object):
     """ Graph data structure, undirected by default. """
 
-    def __init__(self, direction=False):
+    def __init__(self):
         self.n_dirc = {}
-        self.direction = direction
 
     # add a new node
     def add_node(self, n):
         self.n_dirc.update({n:[]})
 
+    # add a new edge
     def add_edge(self, n1, n2):
         if n1 not in self.n_dirc:
             self.add_node(n1)
@@ -21,6 +23,17 @@ class Graph(object):
         
         self.n_dirc[n1].append(n2)
 
+    def find_edge(self, n1, n2):
+        if (n1 not in self.n_dirc) or (n2 not in self.n_dirc):
+            return ()
+        elif n2 in self.n_dirc[n1]:
+            return (n1, n2)
+        elif n1 in self.n_dirc[n2]:
+            return (n2, n1)
+        else:
+            return ()
+
+    # remove node with corresponding edges
     def remove_node(self, n):
         for i in self.n_dirc:
             try:
@@ -33,6 +46,41 @@ class Graph(object):
         except KeyError:
             pass
 
+    # print the graph
     def print_graph(self):
         print(self.n_dirc)
 
+    # generate the graph with matrix
+    def generate_graph(self, M):
+        row, col = M.shape
+        if row != col:
+            print('matrix error!')
+            return
+        else:
+            for n_r in range(row):
+                self.add_node(n_r)
+                for n_c in range(col):
+                    if M[n_r][n_c] == 1 and n_r != n_c and M[n_c][n_r] != 1:
+                        self.add_edge(n_r, n_c)
+        self.print_graph()
+    
+    def generate_random_graph(self, m_size):
+        m = np.random.randint(2, size=(m_size, m_size))
+        self.generate_graph(m)
+
+    def replace_node(self, n1, rn):
+        for i in self.n_dirc:
+            self.n_dirc[i] = [ rn if l==n1 else l for l in self.n_dirc[i] ]
+        
+        self.n_dirc[rn] = self.n_dirc[n1]
+        try:
+            del self.n_dirc[n1]
+        except KeyError:
+            pass
+
+        g.print_graph()
+
+g = Graph()
+g.generate_random_graph(5)
+print(g.find_edge(2,3))
+g.replace_node(1,'a')
