514654e84b8565c13e1f1a6442db16657129d79a
momodupi <momodupi@gmail.com>
2019-02-28 14:08:55 -0500

    Update SimTrans_Simulator.py

--- SimTrans_Simulator.py
+++ SimTrans_Simulator.py
@@ -20,6 +20,7 @@ class SimTrans_Simulator(object):
         self.edge_cost_history = []
         self.edge_decision_history = []
         self.p_list = []
+        self.conv_cost = 0
         self.plot_num = 0
         self.mode = 'normal'
 
@@ -38,6 +39,7 @@ class SimTrans_Simulator(object):
         plt.xlabel('time (s)')
         plt.ylabel('Flow')
         plt.title('Flow of edge {}'.format((n1,n2)))
+        plt.savefig('edge.png', dpi=600)
         
     def plot_all_edges_flow(self, s_time, e_time):
         '''plot flow for each edges from s_time to e_time'''
@@ -48,10 +50,11 @@ class SimTrans_Simulator(object):
         for e in self.edge_flow:
             plt.plot(k, [ i[e] for i in self.edge_flow_history ], label='{}'.format(e))
 
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=int(len(self.edge_flow)/2))
+        plt.legend(loc='upper right')
         plt.title('Flow of all edges')
         plt.xlabel('time (s)')
         plt.ylabel('Flow')
+        plt.savefig('flow.png', dpi=600)
 
     def plot_all_paths_cost(self, s_time, e_time):
         '''plot cost of each path from s_time to e_time'''
@@ -63,10 +66,11 @@ class SimTrans_Simulator(object):
             idx = self.graph.get_paths_cost(self.ori ,self.des).index(e)
             plt.plot(k, [ i[ idx ] for i in self.edge_cost_history ], label='{}'.format(self.graph.get_all_paths(self.ori, self.des)[idx]))
 
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=int(len(self.edge_flow)/2))
+        plt.legend(loc='upper right')
         plt.title('Cost of all paths')
         plt.xlabel('time (s)')
         plt.ylabel('Cost')
+        plt.savefig('cost.png', dpi=600)
 
     def plot_all_paths_decision(self, s_time, e_time):
         '''plot decision of each path from s_time to e_time'''
@@ -78,10 +82,11 @@ class SimTrans_Simulator(object):
             idx = self.graph.get_paths_cost(self.ori ,self.des).index(e)
             plt.plot(k, [ i[ idx ] for i in self.edge_decision_history ], label='{}'.format(self.graph.get_all_paths(self.ori, self.des)[idx]))
 
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=int(len(self.edge_flow)/2))
+        plt.legend(loc='upper right')
         plt.title('Decision for all paths')
         plt.xlabel('time (s)')
         plt.ylabel('Decision')
+        plt.savefig('decision.png', dpi=600)
 
     def plot_show(self):
         '''display all plotted figure'''
@@ -186,9 +191,13 @@ class SimTrans_Simulator(object):
         
         p1 = np.array( p.get_decision(self.ori ,self.des ) )
         p2 = np.array( self.graph.get_paths_cost(self.ori ,self.des) )
-        print( p1.dot( p2 ) )
+        print( p1.dot(p2) )
+        self.conv_cost = p1.dot(p2)
 
-    def run(self, s_time, e_time, init_num, step_num):
+    def get_convergence(self):
+        return self.conv_cost
+
+    def run_once(self, s_time, e_time, init_num, step_num):
         '''run simulator from s_time to e_time'''
         if self.mode == 'wardrop':
             self.simulator_wardrop(s_time, e_time, init_num, step_num)
@@ -196,4 +205,34 @@ class SimTrans_Simulator(object):
             self.simulator_notranstime(s_time, e_time, init_num, step_num)
         else:
             self.simulator_normal(s_time, e_time, init_num, step_num)
+
+
+    def run_sensitivity(self, start_time, end_time, m_f, m_t, m_c, edge_list=[], m_t_range=[], m_c_range=1):
+        K = 10
+        for e in edge_list:
+            self.plot_num = self.plot_num + 1
+            plt.figure(self.plot_num)
+            k = np.arange(m_c[e[0]][e[1]]-K, m_c[e[0]][e[1]]+K)
+            for e_m_t in m_t_range:
+                c_cost = []
+                for e_m_c in range(m_c[e[0]][e[1]]-K, m_c[e[0]][e[1]]+K):
+                    self.edge_flow = {}
+                    self.edge_flow_history = []
+                    self.edge_cost_history = []
+                    self.edge_decision_history = []
+                    self.p_list = []
+                    self.graph.update_w_all_edges(m_f, m_t, m_c)
+                    self.graph.update_w_edge(e[0], e[1], self.graph.convert_w_edge(0, e_m_t, e_m_c))
+                    #self.SimTrans_Simulator(g, 0, 6)
+                    self.set_mode('wardrop')
+                    self.run_once(start_time, end_time, 0, 1)
+                    c_cost.append(self.get_convergence())
+
+                plt.plot(k, c_cost, label='$a={}$'.format(e_m_t))
+
+            plt.legend(loc='lower right')
+            plt.xlabel('b of ({})'.format((e[0], e[1])))
+            plt.ylabel('average cost')
+            plt.title('Average cost of changing constant cost of {}'.format((e[0], e[1])))
+            plt.savefig('avrgcst_{}.png'.format((e[0], e[1])), dpi=600)
             
